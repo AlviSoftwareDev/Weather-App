0899883a31e8d08090c789b55f4897fcfe93aad9
Muhammad Alviansyah <muhammad.alviiansyah@gmail.com>
2023-07-21 14:34:29 +0700

    update weather app

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import { HiLocationMarker } from 'react-icons/hi';
 import { WiHumidity } from 'react-icons/wi';
 
 const weatherIcons = {
-  Clear: './src/assets/clear.png',
-  Rain: './src/assets/rain.png',
-  Snow: './src/assets/snowy.png',
-  Clouds: './src/assets/cloudy.png',
-  Mist: './src/assets/mist.png',
-  Haze: './src/assets/haze.png',
+  Clear: '../src/assets/clear.png',
+  Rain: '../src/assets/rain.png',
+  Snow: '../src/assets/snowy.png',
+  Clouds: '../src/assets/cloudy.png',
+  Mist: '../src/assets/mist.png',
+  Haze: '../src/assets/haze.png',
 };
 
 const App = () => {
